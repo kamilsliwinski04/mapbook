b7380131a96c9362a96065b1ca8fbbcb04797c22
kamilsliwinski04 <168068079+kamilsliwinski04@users.noreply.github.com>
2024-05-23 12:40:26 +0200

    wip: add delete function

--- mapbook.py
+++ mapbook.py
@@ -9,11 +9,42 @@ data_of_users: list = [
 print(f'Witaj {data_of_users[0]["name"]}')
 def read(users:list)->None:
     """
-    this is a function to show users from a list
+    show users from a list
     :param users: a list of users
     :return: None
     """
     for user in users[1:]:
         print(f'twój znajomy:  {user['name']}, opublikował: {user['posts']} postów')
 
+
+# read(data_of_users)
+
+def add_user(users:list)->None:
+    """
+    add user to a list
+    :param users: user list
+    :return: None
+    """
+    name:str=input("Enter your name: ")
+    surname:str=input("Enter your surname: ")
+    posts:int=(input("Enter your number of posts: "))
+    location:str=input("Enter your location: ")
+    new_user: dict={'name': name, 'surname': surname, 'posts': posts, 'location': location}
+    users.append(new_user)
+
+add_user(data_of_users)
 read(data_of_users)
+
+# add_users(data_of_users)
+# read(data_of_users)
+
+def delete_users(users:list)->None:
+    name:str=input('Enter a name of user to remove:')
+    for user in users:
+        if user['name']==name:
+            users.remove(user)
+
+
+
+# delete_user(data_of_users)
+# read(data_of_users)
